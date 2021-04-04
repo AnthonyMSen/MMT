f5fdb81f2f38ad47895e7a48d6b6db00c8775973
AnthonyMSen <1072519956@qq.com>
2021-04-04 17:11:18 +0800

    update clean_code.sh.

--- clean_code.sh
+++ clean_code.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-# curl https://api.day.app/seHy8Styec4YKxvFBtqBte/我运行啦！
+mkdir /MMT
+cp -r $GITHUB_WORKSPACE/ /MMT
+cd /MMT
+rm -r .git .github .idea
+rm clean_code.sh db.sqlite3
+cd /MMT/comment/migrations && rm -v !("__init__.py")
+cd /MMT/myadmin/migrations && rm -v !("__init__.py")
+cd /MMT/users/migrations && rm -v !("__init__.py")
+cd /MMT/video/migrations && rm -v !("__init__.py")
