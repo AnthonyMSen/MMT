0de32363b61f2dc9c628013cdc46ccf08608b614
Anthony_Lap <1072519956@qq.com>
2021-04-07 10:59:07 +0800

    update

--- clean_code.sh
+++ clean_code.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+mkdir -p ~/.ssh/
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+
 mkdir /MMT
-cp -r $GITHUB_WORKSPACE/ /MMT
-cd /MMT
 
-rm -r .git .github .idea
+cd ~
+git clone git@github.com:AnthonyMSen/MMT.git /MMT
+
+cp -rf $GITHUB_WORKSPACE/* /MMT
 rm clean_code.sh db.sqlite3
-
-cp $GITHUB_WORKSPACE/users/migrations/0002_initial.py /MMT/users/migrations/0002_initial.py
 
 sed -i '/ALLOWED_HOSTS/d' /MMT/MeidaManageTool/settings.py
 sed -i '/EMAIL_USE_SSL/d' /MMT/MeidaManageTool/settings.py
 sed -i '/EMAIL_HOST/d' /MMT/MeidaManageTool/settings.py
 sed -i '/EMAIL_PORT/d' /MMT/MeidaManageTool/settings.py
+
+git add .
+git commit -m "auto-update"
+git pull --rebase origin
+git push -u origin
